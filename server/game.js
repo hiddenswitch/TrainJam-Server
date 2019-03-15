ee55d079d2691ee4af371ffc6093b159caac5d30
doctorpangloss <benjamin.s.berman@gmail.com>
2019-03-15 17:10:12 -0500

    Basic game loop

--- server/game.js
+++ server/game.js
@@ -1,6 +1,9 @@
 import {_} from "meteor/underscore";
 import {check} from "meteor/check";
 
+// Stores match resources like timers that cannot be put into databases
+let matchResources = {};
+
 let recipes = {
   'hamburger': ['cooked meat', 'buns'],
   'cheeseburger': ['cooked meat', 'sliced cheese', 'buns']
@@ -23,7 +26,7 @@ let victoryStage = 'victory';
 let stages = [
   {
     stageId: startingStage,
-    matchDuration: 140,
+    matchDuration: 10,
     playerCount: 2,
     orderWinCount: 7,
     // Prefabs that correspond to recipes / objects that must be dropped in the delivery point.
@@ -171,6 +174,136 @@ Meteor.publish('entities', function (matchId, playerId) {
   return Entities.find({matchId: matchId, playerIds: playerId});
 });
 
+function closeMatch(matchId) {
+  _.forEach(matchResources[matchId].timers, timerId => {
+    Meteor.clearTimeout(timerId);
+  });
+
+  delete matchResources[matchId];
+  Matches.remove(matchId);
+  Entities.remove({matchId: matchId});
+}
+
+/**
+ * Starts the stage
+ * @param matchId
+ * @param playerIds
+ * @param stage A stage object
+ */
+function startStage(matchId, playerIds, stage) {
+  matchResources[matchId] = _.extend({timers: []}, matchResources[matchId]);
+  let thisMatchResources = matchResources[matchId];
+  // Cancel all existing timers
+  _.forEach(thisMatchResources.timers, timerId => {
+    Meteor.clearTimeout(timerId);
+  });
+
+  // Remove all existing entities for this match
+  Entities.remove({matchId: matchId});
+
+  // First, show the tutorialization screen with the timer
+  let tutorialDuration = 16;
+  Entities.insert({
+    sceneId: 'tutorialScreen',
+    matchId: matchId,
+    playerIds: playerIds,
+    prefab: 'tutorial screen',
+    values: [tutorialDuration]
+  });
+
+  let tutorialTimerId = Meteor.setTimeout(function () {
+    Entities.insert({
+      sceneId: 'timeLeftClock',
+      matchId: matchId,
+      prefab: 'Time Left Clock',
+      playerIds: playerIds,
+      values: [stage.matchDuration]
+    });
+
+    let endGameEntityId = Entities.insert({
+      sceneId: 'endGame',
+      matchId: matchId,
+      prefab: 'End Game Screen',
+      playerIds: playerIds,
+      // [game over? game won?]
+      bools: [false, false]
+    });
+
+    let endGameTimerId = Meteor.setTimeout(function () {
+      // End of game timer elapsed
+      // Determine if the players won by counting
+      let finishedOrdersCount = Entities.find({matchId: matchId, 'bools.0': true}).count();
+      let didWinGame = finishedOrdersCount >= stage.orderWinCount;
+      let endGameScreenDuration = 8;
+
+      // If the next stage is the victory stage, insert a victory stage
+      if (stage.nextStage === victoryStage) {
+        Entities.insert({
+          sceneId: 'victory',
+          matchId: matchId,
+          playerIds: playerIds,
+          prefab: 'thanks for playing',
+        });
+
+        let delay = 10 * 1000;
+        let victoryScreenTimer = Meteor.setTimeout(function () {
+          closeMatch(matchId);
+        }, delay);
+        thisMatchResources.timers.push(victoryScreenTimer);
+        return;
+      }
+
+      Entities.update(endGameEntityId, {$set: {bools: [true, didWinGame]}});
+      Entities.insert({
+        sceneId: 'endGameClock',
+        matchId: matchId,
+        prefab: 'End Game Clock',
+        playerIds: playerIds,
+        // Show the end game screen for 8 seconds
+        values: [endGameScreenDuration]
+      });
+
+      let endGameScreenTimer = Meteor.setTimeout(function () {
+        startStage(matchId, playerIds, getStage(stage.nextStage, playerIds.length));
+      }, endGameScreenDuration * 1000);
+      thisMatchResources.timers.push(endGameScreenTimer);
+    }, stage.matchDuration * 1000);
+
+    thisMatchResources.timers.push(endGameTimerId);
+
+    // For now, just add all the orders and let the client take care of interpreting their times.
+    _.forEach(stage.orders, order => {
+      Entities.insert({
+        matchId: matchId,
+        prefab: 'order',
+        playerIds: order.playerIds,
+        values: [order.insertionTime, order.warnDuration, order.failureDuration],
+        texts: [order.item].concat(recipes[order.item]),
+        // [finished, failed]
+        bools: [false, false]
+      });
+    });
+  }, tutorialDuration * 1000);
+  thisMatchResources.timers.push(tutorialTimerId);
+}
+
+function createMatch(readyPlayers) {
+  let match = {
+    players: readyPlayers,
+    stage: startingStage,
+    playerCount: readyPlayers.length,
+    round: 1
+  };
+  let matchId = Matches.insert(match);
+  _.extend(match, {_id: matchId});
+  matchResources[matchId] = {};
+  return {match, matchId};
+}
+
+function getStage(stageId, playerCount) {
+  return _.find(stages, s => s.stageId === stageId && s.playerCount === playerCount);
+}
+
 Meteor.methods({
   /**
    * Indicates the player is ready to play
@@ -221,41 +354,20 @@ Meteor.methods({
 
     // All the ready players who are ready in this lobby will be put into the game.
     let readyPlayers = _.pluck(_.filter(lobby.connections, player => player.ready), 'id');
-    let match = {
-      players: readyPlayers,
-      stage: startingStage,
-      playerCount: readyPlayers.length,
-      round: 1
-    };
-    let matchId = Matches.insert(match);
-    _.extend(match, {_id: matchId});
+    let {match, matchId} = createMatch(readyPlayers);
+    let playerIds = _.range(0, readyPlayers.length);
+    let stageId = match.stage;
+    let playerCount = match.playerCount;
+    let stage = getStage(stageId, playerCount);
 
-    let stage = _.find(stages, s => s.stageId === match.stage && s.playerCount === match.playerCount);
-    // TODO: Create the necessary game entities
-    Entities.insert({
-      _id: matchId + '/timeLeftClock',
-      matchId: matchId,
-      prefab: 'Time Left Clock',
-      playerIds: _.range(0, readyPlayers.length),
-      values: [stage.matchDuration]
-    });
-
-    // For now, just add all the orders and let the client take care of interpreting their times.
-    _.forEach(stage.orders, order => {
-      Entities.insert({
-        matchId: matchId,
-        prefab: 'order',
-        playerIds: order.playerIds,
-        values: [order.insertionTime, order.warnDuration, order.failureDuration],
-        texts: [order.item].concat(recipes[order.item]),
-        // [finished, failed]
-        bools: [false, false]
-      });
-    });
-
-    // TODO: Deal with match changes when any players disconnect
+    // Start the first stage
+    startStage(matchId, playerIds, stage);
 
     return matchId;
+  },
+
+  'incrementValue': function (entityId, valueIndex, amount) {
+    return Entities.update(entityId, {$inc: {[`values.${valueIndex}`]: amount}})
   },
 
   'setBool': function (entityId, boolIndex, bool) {
